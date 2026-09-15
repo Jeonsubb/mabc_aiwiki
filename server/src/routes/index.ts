8141b86f12d0ee85473777933247fda2fc6b4890
Jeonsubb <glvlglvl01@naver.com>
2026-09-15 17:22:09 +0900

    feat: add chat message and wiki search APIs

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { nodesRouter } from './nodes';
 import { proposalsRouter } from './proposals';
 import { recordsRouter } from './records';
+import { searchRouter } from './search';
 
 const router = Router();
 router.use('/nodes', nodesRouter);
 router.use('/proposals', proposalsRouter);
 router.use('/records', recordsRouter);
+router.use('/search', searchRouter);
 
 export { router as apiRouter };
